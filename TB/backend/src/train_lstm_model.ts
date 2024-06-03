edf0a891dbe2125378a31ba2c1f5a986d6e2de33
CODEXEDOC <105850164+C0DEXED0C@users.noreply.github.com>
2024-06-02 21:24:38 -0500

    Update train_lstm_model.ts

--- TB/backend/src/train_lstm_model.ts
+++ TB/backend/src/train_lstm_model.ts
@@ -1,1 +1,28 @@
+import * as tf from '@tensorflow/tfjs';
+import '@tensorflow/tfjs-node';
+import { getTradingData } from './utils/data_storage';
 
+async function trainLSTMModel() {
+  const data = getTradingData();
+  const inputs = data.map(d => [d.open, d.close, d.high, d.low]);
+  const labels = data.map(d => d.close);
+
+  const inputTensor = tf.tensor3d(inputs.map(i => i.map(val => [val])), [inputs.length, 4, 1]);
+  const labelTensor = tf.tensor2d(labels, [labels.length, 1]);
+
+  const model = tf.sequential();
+  model.add(tf.layers.lstm({ units: 50, returnSequences: true, inputShape: [4, 1] }));
+  model.add(tf.layers.lstm({ units: 50 }));
+  model.add(tf.layers.dense({ units: 1 }));
+
+  model.compile({ optimizer: 'adam', loss: 'meanSquaredError' });
+
+  await model.fit(inputTensor, labelTensor, {
+    epochs: 100,
+    callbacks: tf.callbacks.earlyStopping({ monitor: 'loss' }),
+  });
+
+  await model.save('file://model');
+}
+
+trainLSTMModel();
