7526ce0217574a654e8a0b473fb3e2d7ef7fb756
CODEXEDOC <105850164+C0DEXED0C@users.noreply.github.com>
2024-06-04 00:40:09 -0500

    Update fetch_holders.ts
    Added logic for fetch holders

--- TB/backend/src/utils/fetch_holders.ts
+++ TB/backend/src/utils/fetch_holders.ts
@@ -1,1 +1,19 @@
+import { Connection, PublicKey } from '@solana/web3.js';
 
+export async function fetchHolders(tokenMint: PublicKey, connection: Connection, batchSize: number = 1000) {
+  let holders = [];
+  let fetched = 0;
+
+  while (true) {
+    const accounts = await connection.getTokenAccountsByMint(tokenMint, { limit: batchSize, offset: fetched });
+    holders = holders.concat(accounts.value.map(account => ({
+      publicKey: new PublicKey(account.pubkey),
+      balance: account.account.data.parsed.info.tokenAmount.uiAmount,
+    })));
+
+    fetched += accounts.value.length;
+    if (accounts.value.length < batchSize) break;
+  }
+
+  return holders;
+}
